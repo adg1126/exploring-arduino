1a75135802bcea6982921575dc5cc1c20d7560ec
adg1126 <gutierrez.almiradyan@gmail.com>
2024-01-25 18:31:03 +0800

    Toggling led light with button

---
+++ 2-4-led_control_with_button/2-4-led_control_with_button.ino
@@ -0,0 +1,18 @@
+// ALL DIGITAL INPUTS MUST HAVE A PULL DOWN RESISTOR CONNECTED TO GROUND
+const int LED = 9;
+const int BUTTON = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(BUTTON) == LOW) {
+    digitalWrite(LED, LOW);
+  } else {
+    digitalWrite(LED, HIGH);
+  }
+}
